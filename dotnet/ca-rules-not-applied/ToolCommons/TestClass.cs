ca93fb433368b234f29194384f3e91fcaff6f186
Sebastian Krysmanski <noreply@manski.net>
2024-09-04 08:36:53 +0200

    Add ca-rules-not-applied reproducer

---
+++ dotnet/ca-rules-not-applied/ToolCommons/TestClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ToolCommons;
+
+public static class TestClass
+{
+    public static int DoSomething(string input)
+    {
+        try
+        {
+            return int.Parse(input);
+        }
+        catch (Exception)
+        {
+            return -1;
+        }
+    }
+}
